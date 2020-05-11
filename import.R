1c1126ba246b044b3696aa80aca42b40b66e1d60
AIG <aig@aigs-imac.speedport_w_724v_typ_a_05011603_06_003>
2020-05-11 17:07:15 +0200

    import analgetics

--- import.R
+++ import.R
@@ -1,10 +1,21 @@
 # prepare Github
-library(usethis)
-?use_github
-edit_r_environ()
-use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))
-
-
+# library(usethis)
+# ?use_github
+# edit_r_environ()
+# use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))
 ##
 
 #import data
+library(tidyverse)
+library(readxl)
+library(here) 
+library(janitor)
+
+repetitive <- read_xlsx(path = here("input/Datenerfassung.xlsx"), range = cell_cols("A:CZ")) %>% 
+  rename(Pat = 'Nr.') %>% 
+  dplyr::select(-matches("Freitext|sonstige")) %>% 
+  janitor::clean_names() %>% 
+  select(-c(besondere_grunde, andere_auffalligkeiten))
+
+pat1 <- data %>% filter(Pat == 1)
+pat1 %>% View()
